b1f06eb2f2b5e6960bb21fa167a80069483695be
Bromar <271488771+BROMAR-ENGINEERING@users.noreply.github.com>
2026-09-08 13:21:59 +1000

    Update comment format in submit-timesheet.js

--- netlify/functions/submit-timesheet.js
+++ netlify/functions/submit-timesheet.js
@@ -1,4 +1,4 @@
-// netlify/functions/submit-timesheet.js — V1.23 — 2026-05-19
+// Submit Timesheet Function - V1.23 - 2026-05-19 - /netlify/functions/submit-timesheet.js
 const { Resend } = require('resend');
 const { createClient } = require('@supabase/supabase-js');
 
